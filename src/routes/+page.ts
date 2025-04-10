737289f0d0b69ab84857c967f2fab6e2b8e7601d
Praveen Kumar Gutnoju <praveen.guntoju@satalia.com>
2025-04-10 17:30:50 +0100

    Update +page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,5 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
-export const prerender = true;
 
 import { store } from '$lib/atomicClient';
 
